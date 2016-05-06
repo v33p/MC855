7519d3156bcd7a5146525d02185aa0f338dec9cd
v33p <v33p@icloud.com>
2016-05-06 18:03:39 -0300

    DEU CERTOOOOOOOO

--- tp02/pageparser.py
+++ tp02/pageparser.py
@@ -1,20 +1,30 @@
-# -*- coding:UTF-8 -*- #
+# -*- coding:utf-8 -*- #
 
 import xml.etree.ElementTree as ET
 
 import re
 import sys
+import unicodedata
 
-patterns = ["Político", "Cientista", "Lutador", "Papa", "Ator", "Futebolista", "Escritor", "Biografia", "esporte/atleta", "Esporte/Atleta", "piloto", "Piloto", "Autor", "Motorista", "Médico", "Jornalista", "Surfista", "Gamer", "Sacerdote", "Exadrista", "Lutador", "Beisebolista", "Treinador", "Astronauta", "Tenista", "Automobilista", "Filósofo", "Árbitro", "Arquiteto", "Jogador", "Boxeador", "Psicólogo", "Voleibol/Jogador", "Criminoso", "Patinador", "Empresario", "Serial Killer", "Nadador", "Arqueiro", "Revolucionário", "Fisiculturista"]
+xml_hc = '{http://www.mediawiki.org/xml/export-0.10/}'
+
+oldpatterns = ["Político", "Cientista", "Lutador", "Papa", "Ator", "Futebolista", "Escritor", "Biografia", "esporte/atleta", "Esporte/Atleta", "piloto", "Piloto", "Autor", "Motorista", "Médico", "Jornalista", "Surfista", "Gamer", "Sacerdote", "Exadrista", "Lutador", "Beisebolista", "Treinador", "Astronauta", "Tenista", "Automobilista", "Filósofo", "Árbitro", "Arquiteto", "Jogador", "Boxeador", "Psicólogo", "Voleibol/Jogador", "Criminoso", "Patinador", "Empresario", "Serial Killer", "Nadador", "Arqueiro", "Revolucionário", "Fisiculturista"]
+
+patterns = ["Politico", "Cientista", "Lutador", "Papa", "Ator", "Futebolista", "Escritor", "Biografia", "esporte/atleta", "Esporte/Atleta", "piloto", "Piloto", "Autor", "Motorista", "Medico", "Jornalista", "Surfista", "Gamer", "Sacerdote", "Exadrista", "Lutador", "Beisebolista", "Treinador", "Astronauta", "Tenista", "Automobilista", "Filosofo", "Arbitro", "Arquiteto", "Jogador", "Boxeador", "Psicologo", "Voleibol/Jogador", "Criminoso", "Patinador", "Empresario", "Serial Killer", "Nadador", "Arqueiro", "Revolucionario", "Fisiculturista"]
 
 def validPage (page):
-    title = page.find ('title').text
+    title = page.find (xml_hc+'title').text
     return (":" not in title)
 
 def isPerson (page):
-    text = page.find ('text').text
+    texts = page.iter (xml_hc+'text')
+    for t in texts:
+	text = t.text
     result = False
+    text = unicodedata.normalize ('NFKD', text).encode('ascii', 'ignore')
+    print type (text)
     for p in patterns:
+	print type (p)
         if ("Info/"+p in text):
             result = True
     return result
@@ -48,7 +58,7 @@ def getWikiPageFromLink (link):
         end = link.index ("#")
 
     link = link[start:end]
-    #link = link.replace ("\\s", "_")
+    link = link.replace (" ", "_")
     link = link.replace (",", "")
     link = link.replace ("&amp;", "&")
 
@@ -56,31 +66,38 @@ def getWikiPageFromLink (link):
 
 def parseXml (xml_file, parse_file):
     # pattern para para encontrar proximos links
-    p = re.compile ('\\[.+?\\[')
+    p = re.compile ('\\[.+?\\]')
     # iniciando arvore xml
-    tree = ET.parse (xml_file)
+    with open (xml_file, 'r') as xml_file2:
+        tree = ET.parse (xml_file2)
     # adiquirindo root
     root = tree.getroot ()
 
     f = open (parse_file, 'w')
-    print ('parse ' + str(xml_file))
+
+    #print ('parse ' + str(xml_file))
+    
     # para cada pagina
-    for page in root.findall ('page'):
-    #for page in root.iter ('page'):
-        print (page.find ('title').text)
+    for page in root.iter (xml_hc+'page'):
+	title = page.find (xml_hc+'title').text
+	title = title.replace (" ", "_")
+        print ('page found: ' + title)
         if (validPage (page)):
-            print (page.find ('title').text)
+            print (title + ' validPage')
             if (isPerson (page)):
-                print (page.find ('title').text)
-                title = page.find ('title').text
-                matcher = p.match (page.find ('text').text)
-                links = p.findall (page.find ('text').text)
+                print (title + ' is a person')
+                texts = page.iter (xml_hc+'text')
+		for t in texts:
+		    text = t.text
+    		text = unicodedata.normalize ('NFKD', text).encode('ascii', 'ignore')
+                matcher = p.match (text)
+                links = p.findall (text)
                 for link in links:
-                    print (link)
+                    print ('link found: ' +link)
                     if (isWikiLink (link) == True):
                         link = getWikiPageFromLink (link)
-                        print (link)
-                        f.write (title + ' ' + link)
+                        print (link + ' added')
+                        f.write (title + ' ' + link+'\n')
     f.close ()
 
 if len (sys.argv) != 3:
