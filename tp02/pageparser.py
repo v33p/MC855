3e466b4665c569c61c31702d8e5f8dcac39b69d9
Lucas Padilha <v33p@icloud.com>
2016-05-06 16:35:59 -0300

    parse correcao

--- tp02/pageparser.py
+++ tp02/pageparser.py
@@ -1,8 +1,6 @@
-!#/bin/bash/
+# -*- coding:UTF-8 -*- #
 
-import xml.etree.ElementTree
-
-from pyspark import SparkContext
+import xml.etree.ElementTree as ET
 
 import re
 import sys
@@ -10,11 +8,11 @@ import sys
 patterns = ["Político", "Cientista", "Lutador", "Papa", "Ator", "Futebolista", "Escritor", "Biografia", "esporte/atleta", "Esporte/Atleta", "piloto", "Piloto", "Autor", "Motorista", "Médico", "Jornalista", "Surfista", "Gamer", "Sacerdote", "Exadrista", "Lutador", "Beisebolista", "Treinador", "Astronauta", "Tenista", "Automobilista", "Filósofo", "Árbitro", "Arquiteto", "Jogador", "Boxeador", "Psicólogo", "Voleibol/Jogador", "Criminoso", "Patinador", "Empresario", "Serial Killer", "Nadador", "Arqueiro", "Revolucionário", "Fisiculturista"]
 
 def validPage (page):
-    title = page.find ('title')
+    title = page.find ('title').text
     return (":" not in title)
 
 def isPerson (page):
-    text = page.find ('text')
+    text = page.find ('text').text
     result = False
     for p in patterns:
         if ("Info/"+p in text):
@@ -23,25 +21,25 @@ def isPerson (page):
 
 def isWikiLink (link):
     start = 1
-    if (link.startswith ("[["):
+    if (link.startswith ("[[")):
         start = 2
-    if (len (link) < start+2 || len (link) > 100):
+    if (len (link) < start+2 or len (link) > 100):
         return False
-    if (link[start] == '#' ||
-        link[start] == '.' ||
-        #link[start] == '\' ||
-        link[start] == '-' ||
+    if (link[start] == '#' or
+        link[start] == '.' or
+        #link[start] == '\' or
+        link[start] == '-' or
         link[start] == '{'):
         return False
-    if (":" in link ||
-        "," in link ||
+    if (":" in link or
+        "," in link or
         "&" in link):
         return False
     return True
 
 def getWikiPageFromLink (link):
     start = 1
-    if (link.startswith("[["):
+    if (link.startswith("[[")):
         start = 2
     end = link.index ("]")
     if ("|" in link):
@@ -63,23 +61,29 @@ def parseXml (xml_file, parse_file):
     tree = ET.parse (xml_file)
     # adiquirindo root
     root = tree.getroot ()
-    
-    f = open (parse_file, 'w')
 
+    f = open (parse_file, 'w')
+    print ('parse ' + str(xml_file))
     # para cada pagina
-    for page in root.iter ('page'):
+    for page in root.findall ('page'):
+    #for page in root.iter ('page'):
+        print (page.find ('title').text)
         if (validPage (page)):
+            print (page.find ('title').text)
             if (isPerson (page)):
-                title = page.find ('title')
-                matcher = p.match (page.find ('text'))
-                links = p.findall (page.find ('text'))
+                print (page.find ('title').text)
+                title = page.find ('title').text
+                matcher = p.match (page.find ('text').text)
+                links = p.findall (page.find ('text').text)
                 for link in links:
+                    print (link)
                     if (isWikiLink (link) == True):
                         link = getWikiPageFromLink (link)
+                        print (link)
                         f.write (title + ' ' + link)
+    f.close ()
 
-if __name__ == "__main__":
-    if len (sys.argv) != 3:
-        print "wrong parameters"
-        exit (-1)
-    parseXml (sys.argv[1], sys.argv[2])
+if len (sys.argv) != 3:
+    print "wrong parameters"
+    exit (-1)
+parseXml (sys.argv[1], sys.argv[2])
